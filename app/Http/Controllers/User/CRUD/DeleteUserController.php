4bc02f54b6f44efb68edab60b2f08838fd1d6303
Caio Willwohl <caiowillwohl@pop-os.localdomain>
2023-11-15 18:54:14 -0300

    crud-users: creating user controllers

---
+++ app/Http/Controllers/User/CRUD/DeleteUserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\User\CRUD;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DeleteUserController extends Controller
+{
+    //
+}
